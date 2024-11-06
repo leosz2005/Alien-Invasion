6905aeb90204575e87e8af12e19aec15286a375d
Leonardo Sanchez <leonardosanchezzerlin2005@gmail.com>
2024-11-06 09:16:21 +0100

    Generamos archivo de que guarda la puntuación maxima

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+HIGH_SCORE_FILE = 'high_score.json'
 class GameStats:
     """ Sigue las estadistas del Alien Invasion"""
 
@@ -9,7 +13,22 @@ class GameStats:
         self.game_active = False
 
         # La puntuación no debería reiniciarse
-        self.high_score = 0
+        if os.path.exists(HIGH_SCORE_FILE):
+            with open(HIGH_SCORE_FILE, 'r') as f:
+                content = f.read()
+                if content:
+                    self.high_score = json.loads(content)
+                else:
+                    self.high_score = 0
+                
+        else:
+            with open(HIGH_SCORE_FILE, 'w') as f:
+                self.high_score = 0
+                json.dump(self.high_score, f)
+
+    def save_high_score(self):
+        with open(HIGH_SCORE_FILE, 'w') as f:
+            json.dump(self.high_score, f) 
 
     def reset_stats(self):
         """ Inicializa las estadísticas que pueden cambiar durante el juego"""
